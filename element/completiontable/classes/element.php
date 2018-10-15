516c041a1d0fac92120530a51d2abf45e2aa311e
Nicolas Roeser <nicolas.roeser@uni-ulm.de>
2018-10-15 10:58:32 +0200

    LMS-2994 (2/2) - Only select completed activities
    Fix bug in testing for activity completion: ignore entries in database
    table 'course_modules_completion' that mark the activity as not
    completed. Only handle the three completed states.

--- element/completiontable/classes/element.php
+++ element/completiontable/classes/element.php
@@ -827,14 +827,16 @@ class element extends \mod_customcert\element {
                 $select = 'coursemoduleid = :cmid AND userid = :userid AND (
                         completionstate = :complstate1 OR
                         completionstate = :complstate2
+                        completionstate = :complstate2 OR
+                        completionstate = :complstate3
                     )';
                 $params = array(
                     'cmid' => $cmid,
                     'userid' => $user->id,
                     'complstate1' => COMPLETION_COMPLETE,
-                    'complstate2' => COMPLETION_COMPLETE_PASS
-                    /* We could also use negative matching with completionstate<>COMPLETION_INCOMPLETE AND
-                    * completionstate<>COMPLETION_COMPLETE_FAIL;
+                    'complstate2' => COMPLETION_COMPLETE_PASS,
+                    'complstate3' => COMPLETION_COMPLETE_FAIL
+                    /* We could also use negative matching with completionstate<>COMPLETION_INCOMPLETE;
                     * the solution chosen here might make the code easier to understand or easier to grep and is more aligned with
                     * other parts of the Moodle codebase.
                     */
