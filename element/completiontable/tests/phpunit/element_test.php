44816d4d71dafcc8acd742c66993756a024cea1c
Nicolas Roeser <nicolas.roeser@uni-ulm.de>
2018-10-17 16:36:22 +0200

    LMS-2994, follow-up - only accept activity completion if not failed
    Adjust meaning of activity completion in case of activities that can be
    passed or failed, like assignments: do not include entries in the
    completion table element if the student has failed the activity. It is
    still included if the activity is passed or completion is not combined
    with a pass/fail marking.
    
    This commit adjusts the non-test code to behave in the expected way
    described above.

--- element/completiontable/tests/phpunit/element_test.php
+++ element/completiontable/tests/phpunit/element_test.php
@@ -73,7 +73,7 @@ class customcertelement_completiontable_element_test extends advanced_testcase {
                "numranges": 0,
                "dateranges": []
                }'; //JSON format.
-	
+
         $element = \mod_customcert\element_factory::get_element_instance($elementdata);
 
         return $element;
