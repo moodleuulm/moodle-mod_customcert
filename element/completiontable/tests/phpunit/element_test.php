4e6ca7b8422a11d1b5723c0dc60f2d97bfc3100f
Alina Antonova <alina.antonova@uni-ulm.de>
2025-06-23 15:50:53 +0200

    Update merge mod_customcert

--- element/completiontable/tests/phpunit/element_test.php
+++ element/completiontable/tests/phpunit/element_test.php
@@ -72,8 +72,15 @@ class customcertelement_completiontable_element_test extends advanced_testcase {
                "fallbackstring" : "' . MOD_CUSTOMCERT_TESTS_COMPLETED_FALLBACKSTRING . '",
                "numranges": 0,
                "dateranges": []
-               }'; //JSON format.
-
+	       }'; //JSON format.
+	
+	    $elementdata->element = 'completiontable';
+        $elementdata->data = '{
+               "content" : "{completion:' . $activity->get_course_module()->id . '}",
+               "fallbackstring" : "",
+               "numranges": 0,
+               "dateranges": []
+            }'; // JSON format.
         $element = \mod_customcert\element_factory::get_element_instance($elementdata);
 
         return $element;
